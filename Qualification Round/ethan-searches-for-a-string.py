39426b273b43cc15c91b0b14221fc0b8dd4f954a
Allen Liu <kamyu104@gmail.com>
2018-07-10 08:27:49 +0800

    update

--- Qualification Round/ethan-searches-for-a-string.py
+++ Qualification Round/ethan-searches-for-a-string.py
@@ -11,9 +11,10 @@ def ethan_searches_for_a_string():
     A = raw_input().strip()
     i = A.find(A[0], 1)
     if i != -1:
-        for j in xrange(i, len(A)):
-            if A[j] != A[j%i]:
-                return A[:i]+A
+        for j in xrange(i, len(A), i):
+            l = min(j+i, len(A))-j
+            if A[j:j+l] != A[:l]:
+                return A
     return "Impossible"
 
 for case in xrange(input()):

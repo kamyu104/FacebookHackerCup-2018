06c6ebf2515a5744ff6eeb4a19a264ae08472279
Allen Liu <kamyu104@gmail.com>
2018-07-10 08:18:46 +0800

    update

--- Qualification Round/ethan-searches-for-a-string.py
+++ Qualification Round/ethan-searches-for-a-string.py
@@ -3,17 +3,17 @@
 # Facebook Hacker Cup 2018 Qualification Round - Ethan Searches for a String
 # https://www.facebook.com/hackercup/problem/1153996538071503/
 #
-# Time:  O(N^2)
+# Time:  O(N)
 # Space: O(1)
 #
 
 def ethan_searches_for_a_string():
     A = raw_input().strip()
-    for i in xrange(1, len(A)):
-        if A[i] == A[0]:
-            for j in xrange(len(A)):
-                if A[j] != A[j%i]:
-                    return A[:i]+A
+    i = A.find(A[0], 1)
+    if i != -1:
+        for j in xrange(len(A)):
+            if A[j] != A[j%i]:
+                return A[:i]+A
     return "Impossible"
 
 for case in xrange(input()):

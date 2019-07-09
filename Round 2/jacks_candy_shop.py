95f77a79186f311c5cb5a49d6a82a3d2241873f6
kamyu <kamyu104@users.noreply.github.com>
2019-07-09 21:26:47 +0800

    Update jacks_candy_shop.py

--- Round 2/jacks_candy_shop.py
+++ Round 2/jacks_candy_shop.py
@@ -20,24 +20,30 @@ def jacks_candy_shop():
 
     result = 0
     max_heaps = [[] for _ in xrange(N)]
-    stk = [(0, 0, None)]
+    cnt = 0
+    stk = [(0, (0))]
     while stk:
-        step, i, j = stk.pop()
+        step, args = stk.pop()
         if step == 0:
-            stk.append((2, i, None))
+            i = args
+            stk.append((2, (i)))
             for j in adj[i]:
-                stk.append((1, i, j))
-                stk.append((0, j, None))
+                stk.append((1, (i, j)))
+                stk.append((0, (j)))
         elif step == 1:
+            i, j = args
             if len(max_heaps[i]) > len(max_heaps[j]):
                 max_heaps[i], max_heaps[j] = max_heaps[j], max_heaps[i]
             while max_heaps[j]:
                 heappush(max_heaps[i], heappop(max_heaps[j]))
         else:
+            i = args
             heappush(max_heaps[i], -i)
             while count[i] and max_heaps[i]:
                 count[i] -= 1
                 result += -heappop(max_heaps[i])
+            cnt += 1
+            print cnt
     return result
 
 for case in xrange(input()):

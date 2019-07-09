98c4ec0281faa01ce4f4a9043f325caaa033b091
kamyu <kamyu104@users.noreply.github.com>
2019-07-09 22:51:29 +0800

    Update jacks_candy_shop.py

--- Round 2/jacks_candy_shop.py
+++ Round 2/jacks_candy_shop.py
@@ -10,19 +10,19 @@
 from heapq import heappush, heappop
 from functools import partial
 
-def divide_tree(stk, adj, count, max_heaps, i, result):
-    stk.append(partial(conquer_tree, count, max_heaps, i, result))
+def divide(stk, adj, count, max_heaps, i, result):
+    stk.append(partial(conquer, count, max_heaps, i, result))
     for j in adj[i]:
-        stk.append(partial(merge_subtree, max_heaps, i, j))
-        stk.append(partial(divide_tree, stk, adj, count, max_heaps, j, result))
+        stk.append(partial(merge, max_heaps, i, j))
+        stk.append(partial(divide, stk, adj, count, max_heaps, j, result))
 
-def merge_subtree(max_heaps, i, j):
+def merge(max_heaps, i, j):
     if len(max_heaps[i]) < len(max_heaps[j]):
         max_heaps[i], max_heaps[j] = max_heaps[j], max_heaps[i]
     while max_heaps[j]:
         heappush(max_heaps[i], heappop(max_heaps[j]))
 
-def conquer_tree(count, max_heaps, i, result):
+def conquer(count, max_heaps, i, result):
     heappush(max_heaps[i], -i)
     while count[i] and max_heaps[i]:
         count[i] -= 1
@@ -40,7 +40,7 @@ def jacks_candy_shop():
     result = [0]
     max_heaps = [[] for _ in xrange(N)]
     stk = []
-    stk.append(partial(divide_tree, stk, adj, count, max_heaps, 0, result))
+    stk.append(partial(divide, stk, adj, count, max_heaps, 0, result))
     while stk:
         stk.pop()()
     return result[0]

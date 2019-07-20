47af3d0f6e718109ce8a4a8e4dcb5485e0a96ba3
Allen Liu <kamyu104@gmail.com>
2019-07-20 23:52:07 +0800

    updatE

---
+++ Round 3/jammin.py
@@ -0,0 +1,44 @@
+# Copyright (c) 2019 kamyu. All rights reserved.
+#
+# Facebook Hacker Cup 2018 Round 3 - Jammin'
+# https://www.facebook.com/hackercup/problem/1851349144951409/
+#
+# Time:  O(N)
+# Space: O(1)
+#
+
+def jammin():
+    C = list(raw_input())
+    C += [' ', ' ']
+
+    result, curr, past, past_reachable = 0, 0, 0, 0
+    i = 0
+    while i < len(C):
+        if C[i] == '*':
+            curr += 1
+            if  curr >= 2 or past_reachable:
+                curr += past
+                past, past_reachable = 0, 0
+            result = max(result, curr)
+        elif C[i] == '#' and C[i+1] != '#':  # 1 barrier
+            if curr < 2:
+                if C[i+1] == '*':
+                    past_reachable = 0
+                elif curr:  # place one, and pick back later if possible
+                    curr -= 1
+                    past += 1
+                    past_reachable = 1
+                else:
+                    break  # stop
+        elif C[i] == '#' and C[i+1] == '#' and C[i+2] != '#':  # 2 barriers
+            if curr and C[i+2] == '*':  # swap
+                i += 2
+            else:
+                break  # stop
+        elif C[i] == '#' and C[i+1] == '#' and C[i+2] == '#':  # 3 barriers
+            break  # stop
+        i += 1
+    return result
+
+for case in xrange(input()):
+    print 'Case #%d: %s' % (case+1, jammin())
